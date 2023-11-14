b26df3127d9e71f9fdbd7cce06e2fc2934698755
David <davidmv1185@gmail.com>
2023-11-13 19:22:07 -0800

    test

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ const playerContainer = document.getElementById('all-players-container');
 const newPlayerFormContainer = document.getElementById('new-player-form');
 
 // Add your cohort name to the cohortName variable below, replacing the 'COHORT-NAME' placeholder
-const cohortName = 'YOUR COHORT NAME HERE';
+const cohortName = '2308-acc-pt-web-pt-b';
 // Use the APIURL variable for fetch requests
-const APIURL = `https://fsa-puppy-bowl.herokuapp.com/api/${cohortName}/`;
+const APIURL = `https://fsa-puppy-bowl.herokuapp.com/api/2308-acc-pt-web-pt-b/`;
 
 /**
  * It fetches all players from the API and returns them
